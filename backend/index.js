eeec68730edc612534079330a0d07bdc56725f0b
Дагвадорж Гантөмөр <24LP0751@G401-CC0158.local>
2024-11-13 12:51:27 +0800

    backend frontend holbson

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 import express from "express";
+import cors from "cors";
 
 const server = express();
 const PORT = 8000;
 
-server.get("/",(req, res) => {
-  res.send("hello world");
+server.use(cors());
+
+server.get("/", (request, response) => {
+  response.send(["davga", "dorj"]);
 });
+
 server.listen(PORT, () => {
-  console.log(`http://localhost:${PORT} ajilaj bn`);
+  console.log(`http://localhost:${PORT} ajillaj ehllee`);
 });
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,21 @@
+"use client";
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/");
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -12,8 +27,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <p>hi </p>
-
+        <p>Food delivery project aaa</p>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
