e626a728ff8b6a440f8c8144e915630c432b3c1c
Дагвадорж Гантөмөр <24LP0751@G305-CC0241.local>
2024-11-18 19:47:50 +0800

    map

--- frontend/src/app/comp/Map.tsx
+++ frontend/src/app/comp/Map.tsx
@@ -1,23 +1,61 @@
-import React from "react";
+"use client"
+
+import React, { useEffect, useRef } from "react"; 
 import { PiStarFourFill } from "react-icons/pi";
+import { Loader } from "@googlemaps/js-api-loader";
+
 const Map = () => {
+  const mapRef = useRef(null);  
+  const mapInstance = useRef(null); 
+
+  useEffect(() => {
+    
+    const initMap = async () => {
+      const loader = new Loader({
+        apiKey: "AIzaSyBeK6kNAnqtWp2xZpPHmcXOjESpg5cu6ek",
+        version: "weekly",
+      });
+
+      try {
+     
+        const { Map } = await loader.importLibrary("maps");
+        
+        
+        const mapOptions = {
+          center: { lat: 47.91996922842304, lng: 106.91756534492119 },
+          zoom: 13,
+        };
+
+        
+        if (mapRef.current && !mapInstance.current) {
+          mapInstance.current = new Map(mapRef.current, mapOptions);
+        }
+      } catch (error) {
+        console.error("Google Maps API-ийг ачаалах үед алдаа гарлаа:", error);
+      }
+    };
+
+    
+    initMap();
+  }, []);  
+
   return (
-    <div className="flex  justify-center pt-[61px]">
+    <div className="flex justify-center pt-[61px]">
       <div className="flex flex-col gap-10 pb-10">
-        <div className="w-[1200px] h-[616px] border border-black rounded-xl flex items-center justify-center p ">
-          map
+        <div className="  ">
+          <div ref={mapRef} className="w-full h-[616px] border border-black rounded-xl flex items-center justify-center"></div>
         </div>
         <div className="flex items-center gap-2">
           <div>
-            <PiStarFourFill className="w-8 h-8 text-[#18BA51] " />
+            <PiStarFourFill className="w-8 h-8 text-[#18BA51]" />
           </div>
           <div className="text-[22px] font-bold">
             Хүргэлтийн бүс дэх хаягууд
           </div>
         </div>
         <div className="flex gap-4">
-          <div className="w-[588px] h-[388px] rounded-2xl shadow-xl border  px-6 py-6">
-            <div className="h-[56px] w-[540px] border-l-0 border-t-0 border-r-0  border-b-[#18BA51] border-[2px] text-[20px] font-bold ">
+          <div className="w-[588px] h-[388px] rounded-2xl shadow-xl border px-6 py-6">
+            <div className="h-[56px] w-[540px] border-l-0 border-t-0 border-r-0 border-b-[#18BA51] border-[2px] text-[20px] font-bold ">
               А бүс
             </div>
             <div className="flex pt-4">
@@ -41,8 +79,8 @@ const Map = () => {
               </div>
             </div>
           </div>
-          <div className="w-[588px] h-[388px] rounded-2xl shadow-xl border  px-6 py-6">
-            <div className="h-[56px] w-[540px] border-l-0 border-t-0 border-r-0  border-b-[#18BA51] border-[2px] text-[20px] font-bold ">
+          <div className="w-[588px] h-[388px] rounded-2xl shadow-xl border px-6 py-6">
+            <div className="h-[56px] w-[540px] border-l-0 border-t-0 border-r-0 border-b-[#18BA51] border-[2px] text-[20px] font-bold ">
               А бүс
             </div>
             <div className="flex pt-4">
